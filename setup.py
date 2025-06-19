470e8939d7a14d28aadec2fec732cbdb8f85c2f0
revETE <87857295+revETE@users.noreply.github.com>
2025-06-19 23:58:47 +0300

    FET: Make package

---
+++ setup.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*
+from setuptools import setup
+
+setup(
+    name="dqn",
+    version="0.1.0",
+    packages=["dqn"],
+    package_dir={"dqn": "dqn"},
+    entry_points={
+        "console_scripts": [
+            "dqn-train = dqn.cli:train",
+            "dqn-evaluate = dqn.cli:eval",
+        ]
+    },
+)
